53703cd0383a9d633f6ad8cfc96840c48ad997f6
Antonio Estela <antonioestela73@gmail.com>
2021-04-08 14:49:17 -0500

    Fixed buttons

--- 0x09-unity_ar_business_card/Assets/Scripts/ButtonController.cs
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonController.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonController : MonoBehaviour
-{
-    public void OpenLinkedIn()
+{    public void OpenLinkedIn()
     {
+        Debug.Log("OPEN URl");
         Application.OpenURL("https://www.linkedin.com/in/antonio-jos%C3%A9-estela-7b2a64156/");
     }
 
     public void OpenTwitter()
     {
+        Debug.Log("OPEN URl");
         Application.OpenURL("https://twitter.com/Antonio__Estela");
     }
 
     public void OpenGitHub()
     {
+        Debug.Log("OPEN URl");
         Application.OpenURL("https://github.com/AntonioEstela");
     }
 
