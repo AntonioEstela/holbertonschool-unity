37fb40fda2d803600615d8814e7ac7a909c41adf
Antonio Estela <antonioestela73@gmail.com>
2021-04-08 12:12:47 -0500

    First Upload

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    public void OpenLinkedIn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/antonio-jos%C3%A9-estela-7b2a64156/");
+    }
+
+    public void OpenTwitter()
+    {
+        Application.OpenURL("https://twitter.com/Antonio__Estela");
+    }
+
+    public void OpenGitHub()
+    {
+        Application.OpenURL("https://github.com/AntonioEstela");
+    }
+
+    public void SentEmail()
+    {
+        
+    }
+}
