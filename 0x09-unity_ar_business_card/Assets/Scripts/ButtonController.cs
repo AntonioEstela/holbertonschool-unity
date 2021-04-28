c8a60bcd8e50373a506bbc759a64914923ff6a9b
Antonio Estela <antonioestela73@gmail.com>
2021-04-28 15:21:19 -0500

    Some fixes

--- 0x09-unity_ar_business_card/Assets/Scripts/ButtonController.cs
+++ 0x09-unity_ar_business_card/Assets/Scripts/ButtonController.cs
@@ -5,24 +5,21 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {    public void OpenLinkedIn()
     {
-        Debug.Log("OPEN URl");
         Application.OpenURL("https://www.linkedin.com/in/antonio-jos%C3%A9-estela-7b2a64156/");
     }
 
     public void OpenTwitter()
     {
-        Debug.Log("OPEN URl");
         Application.OpenURL("https://twitter.com/Antonio__Estela");
     }
 
     public void OpenGitHub()
     {
-        Debug.Log("OPEN URl");
         Application.OpenURL("https://github.com/AntonioEstela");
     }
 
-    public void SentEmail()
+    public void OpenInstagram()
     {
-        
+        Application.OpenURL("https://www.instagram.com/antonioestela73/");
     }
 }
